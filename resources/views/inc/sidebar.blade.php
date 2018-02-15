faf0776d74f681eb1182436b459e9581779e86e8
Sasi <sasikaran7@gmail.com>
2018-02-15 22:27:34 +0800

    initial commit

---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,5 @@
+@section('sidebar')
+<div class="sidebar">
+		This is side bar 
+		@show
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Laravel Example Site</title>
+</head>
+<body>
+
+@yield('content')
+
+@include('inc.sidebar')
+
+</body>
+</html>
